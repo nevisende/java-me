8243afe808c106c7b53a21947a04b15fa1c01383
Furkan Denizhan <84407337+nevisende@users.noreply.github.com>
2022-12-18 18:55:23 +0300

    Feat: add classes of Palindrome String

---
+++ state-3/palindrome-string/src/com/nevisende/ps/Main.java
@@ -0,0 +1,21 @@
+package com.nevisende.ps;
+
+public class Main {
+    static boolean isPalindrome(String str) {
+        int i = 0;
+        int j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("nacican"));
+        System.out.println(isPalindrome("abla"));
+
+    }
+}
